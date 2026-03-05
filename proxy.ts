44fe3586a949238be29659b34b148e416ad13309
Denys Ovcharov <denisovcharov88@gmail.com>
2026-03-06 00:30:29 +0100

    'update'

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 const privateRoutes = ["/profile", "/notes"];
 const publicRoutes = ["/sign-in", "/sign-up"];
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
